15a2baac304d8f9385d8868c4b017bfa2fb989af
qaz6750 <2838632319@qq.com>
2023-07-20 00:19:39 +0800

    Project: Build: Set up cleaning before each build
    Signed-off-by: qaz6750 <2838632319@qq.com>

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
+bash clean.sh
 GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p sm7150Pkg/qcom-sdmmagpie.dsc -b RELEASE
 gzip -c < workspace/Build/sdmmagpie/RELEASE_GCC5/FV/SM7150_UEFI.fd >uefi.img
 cat qcom-sdmmagpie.dtb >>uefi.img
